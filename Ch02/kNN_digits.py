d82d9e6aa673302745f4079e0f16be384e4173d0
qushupeng <fallenadonis@gmail.com>
2017-07-06 17:42:52 +0800

    更新kNN手写数字识别代码

--- Ch02/kNN_digits.py
+++ Ch02/kNN_digits.py
@@ -1,69 +1,80 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul  4 15:46:50 2017
+Created on Thu Jul  6 16:57:35 2017
 
 @author: sq186002
 """
 
-
-import os
 import numpy as np
+import os
 import operator
-'''
 
-'''
-def importFile2Array(filename):
-    '''
-    filename:外部文件路径及文件名，格式（windows）为'D:\XXX\XXXXX.TXT'
-    split:分隔符，默认为tab
-    '''
+def file2Array(filename):
+    
     outputList = []
-    #将文本内容按行写入list
     with open(filename,'r') as importFile:
-        x = importFile.readlines()
-        for line in x:
-            outputList.append(list(line.replace('\n','')))
+        lines = importFile.readlines()
+        for line in lines:
+            outputList.extend(list(line.replace('\n','')))
     
     return np.array(outputList).astype(int)
-
-
-def fileDistance(fileName1,fileName2):
-    pass
-
-if __name__ == '__main__':
     
-    trainingFilePath = "E:\\GitHub\\MLinAction\\Ch02\\SampleData\\digits\\trainingDigits"
-    testFilePath = "E:\\GitHub\\MLinAction\\Ch02\\SampleData\\digits\\testDigits"
+def files2Array(pathname):
 
-    trainingFileName = []
-    for file in os.listdir(trainingFilePath):
-        trainingFileName.append([trainingFilePath+"\\"+file,file[0]])
+    dataset = []
+    labels = []
+    for file in os.listdir(pathname):
+        dataset.append(file2Array(pathname+'\\'+file))
+        labels.append(file[0])
         
-    testFileName = []
-    for file in os.listdir(testFilePath):
-        testFileName.append([testFilePath+"\\"+file,file[0]])
+    return np.array(dataset),np.array(labels)
 
-    print(trainingFileName[0][0])
-    
-    file1 = importFile2Array(trainingFileName[0][0])
-    print(file1)
-    
-    classCount = {}
-    
-    for filePath1 in testFileName:
-        fileName1 = filePath1[0][0]
-        for filePath2 in trainingFileName:
-            fileName2 = filePath2[0]
-            distance = fileDistance(fileName1,fileName2)
-            classCount[filePath2[1]] = classCount.get(filePath2[1],0) + 1
+def classifyKNN(inX,dataSet,labels,k):
+    '''
+    kNN: k Nearest Neighbors
 
-        sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
-    
+    Input:      
+            inX: vector to compare to existing dataset (1xN)
+            dataSet: size m data set of known vectors (NxM)
+            labels: data set labels (1xM vector)
+            k: number of neighbors to use for comparison (should be an odd number)
             
+    Output:     
+            the most popular class label
+    '''
+    #矩阵做差
+    diffMat = np.tile(inX, (dataSet.shape[0],1)) - dataSet
+    #求欧氏距离
+    distances = ((diffMat**2).sum(axis=1))**0.5
 
+    sortedDistIndicies = distances.argsort()
+    classCount={}
 
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
 
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+        
+    
+trainingFilePath = "E:\\GitHub\\MLinAction\\Ch02\\SampleData\\digits\\trainingDigits\\0_0.txt"
 
+fileArray = file2Array(trainingFilePath)
+print(fileArray)
 
+pathname = "E:\\GitHub\\MLinAction\\Ch02\\SampleData\\digits\\trainingDigits"
 
+dataset,labels = files2Array(pathname)
+print(dataset)
+print(labels)
 
+pathname2 = "E:\\GitHub\\MLinAction\\Ch02\\SampleData\\digits\\testDigits"
+inX,answers = files2Array(pathname2)
+
+count = 0
+for i in range(len(inX)):
+    if classifyKNN(inX[i],dataset,labels,3) == answers[i]:
+        count += 1
+
+print(str(count/len(inX) * 100) + "%")
